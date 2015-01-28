107eda37df04a0d13728bf351f6cb1f68c6826bd
Kozo Nishida <knishida@riken.jp>
2015-01-28 15:23:33 +0900

    add inplace

--- Formula/pathvisio.rb
+++ Formula/pathvisio.rb
@@ -8,6 +8,7 @@ class Pathvisio < Formula
   depends_on :java => "1.7"
 
   def install
+    inreplace "pathvisio.sh", "$(dirname $0)", libexec
     libexec.install %W[LICENSE-2.0.txt NOTICE.txt pathvisio.jar pathvisio.sh]
     bin.install_symlink libexec/"pathvisio.sh" => "pathvisio"
     doc.install "readme.txt"
